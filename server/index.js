eda492986fcdbbd918a62b63f76ccf42886f0dc1
pea-nut-z <topeanutz@gmail.com>
2022-06-01 14:49:19 -0400

    refactor: removed unuse code

--- server/index.js
+++ server/index.js
@@ -8,12 +8,6 @@ const server = require("socket.io")(http, {
   },
 });
 
-const path = require("path");
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 const PORT = process.env.PORT || 3001;
 http.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
 
