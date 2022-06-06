551d7c9f022ae53667e589be9c6adca2500e17f2
pea-nut-z <topeanutz@gmail.com>
2022-06-06 16:26:45 -0400

    refactor: added proxy

--- client/src/contexts/socket.js
+++ client/src/contexts/socket.js
@@ -1,4 +1,4 @@
 import socketio from "socket.io-client";
 import React from "react";
 export const SocketContext = React.createContext();
-export const socket = socketio.connect("http://localhost:3001", { forceNew: true });
+export const socket = socketio.connect("/", { forceNew: true });
