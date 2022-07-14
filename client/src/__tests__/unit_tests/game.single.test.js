a493d561d098eeecd60f6ef525fd315b35531c2a
pea-nut-z <topeanutz@gmail.com>
2022-07-13 23:44:15 -0400

    test: updated test

--- client/src/__tests__/unit_tests/game.single.test.js
+++ client/src/__tests__/unit_tests/game.single.test.js
@@ -4,24 +4,20 @@ import "@testing-library/jest-dom";
 import Game from "../../screen/Game";
 
 describe("Single player mode", () => {
-  const player = "Tester";
-  const incrementData = jest.fn();
-  const toggleGameModeCb = jest.fn();
+  const props = {
+    userName: "Tester",
+    game: "single",
+    incrementData: jest.fn(),
+    toggleGameModeCb: jest.fn(),
+  };
   let wrapper;
 
   beforeAll(() => {
-    wrapper = shallow(
-      <Game
-        userName={player}
-        game={"single"}
-        incrementData={incrementData}
-        toggleGameModeCb={toggleGameModeCb}
-      />
-    );
+    wrapper = shallow(<Game {...props} />);
   });
 
   it("renders initial setup", () => {
-    expect(wrapper.find({ "data-testid": "p1Name" }).text()).toEqual(player);
+    expect(wrapper.find({ "data-testid": "p1Name" }).text()).toEqual(props.userName);
     expect(wrapper.find({ "data-testid": "p2Name" }).text()).toEqual("Peanutbot");
     expect(wrapper.find({ "data-testid": "round" }).text()).toEqual("Round: 1");
     expect(wrapper.find({ "data-testid": "score1" }).text()).toEqual("0");
