b139456526f34760c41eca640745a46002b423a7
Peanut <topeanutz@gmail.com>
2022-10-08 00:29:51 -0400

    feat:add guest login

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -3,28 +3,47 @@ import { Form, Card, Alert } from "react-bootstrap";
 import { CustomButton, CustomLink } from "../UI";
 import { useAuth } from "../contexts/AuthContext";
 import { useHistory } from "react-router-dom";
+import ShortUniqueId from "short-unique-id";
 
 export default function Login() {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const { login } = useAuth();
+  const { signup, login } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-
+  
+  const guestLogin = async () => {
+    const uid = new ShortUniqueId({ length: 6 })();
     try {
       setError("");
       setLoading(true);
-      await login(emailRef.current.value, passwordRef.current.value);
-      history.push("/");
+        const userName = "Guest " + uid
+        await signup(uid + "@gmail.com", uid).then((cred) => {
+          cred.user.updateProfile({
+            displayName: userName,
+          });
+        });
+      history.push("/", {userName});
     } catch {
       setLoading(false);
       setError("Failed to log in");
     }
   }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setError("");
+      setLoading(true);
+        await login(emailRef.current.value, passwordRef.current.value);
+        history.push("/");
+    } catch {
+      setLoading(false);
+      setError("Failed to log in");
+    }
+}
 
   return (
     <div className="container">
@@ -41,7 +60,8 @@ export default function Login() {
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control data-testid="loginEmailInput" type="email" ref={emailRef} required />
+              <Form.Control data-testid="loginEmailInput" type="email"
+                ref={emailRef} required />
             </Form.Group>
             <Form.Group id="password">
               <Form.Label>Password</Form.Label>
@@ -54,6 +74,9 @@ export default function Login() {
             </Form.Group>
             <CustomButton testid="login" text="Log In" disabled={loading} type="submit" />
           </Form>
+          <CustomButton testid="guest-login" text="Guest Login" type="button"
+        func={guestLogin}
+        />
           <CustomLink testid="forgotPassword" text="Forgot Password?" to="/forgot-password" />
         </Card.Body>
       </Card>
