cf17d15fe27a3035e7e0506552d98be2c2aa0a9a
Peanut <topeanutz@gmail.com>
2022-10-30 14:13:53 -0400

    misc: change server uri

--- client/src/contexts/socket.js
+++ client/src/contexts/socket.js
@@ -1,8 +1,8 @@
 import socketio from "socket.io-client";
 import React from "react";
 
-
-const endpoint = process.env.NODE_ENV === "production" ? "https://connect-four-server.up.railway.app" : "/"
+const endpoint =
+  process.env.NODE_ENV === "production" ? "https://connect-four-server.onrender.com/" : "/";
 
 export const socket = socketio.connect(endpoint, {
   reconnection: false,
